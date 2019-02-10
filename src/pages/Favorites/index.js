b007378e14988444662444becaf899e131a76e93
gabifagundes <gabiipfagundes@gmail.com>
2019-02-10 15:48:32 -0200

    Adiçao de emptyState para página de favoritos

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Arrow from "../../assets/Icons/icon-arrow-white.svg";
+import NotFound from "../../assets/Illustrations/not-found2.svg";
 import Card from "../../modules/Card";
 
 class Favorites extends Component {
@@ -21,6 +22,8 @@ class Favorites extends Component {
           return <Card key={item.id} data={item} {...this.props} />;
         })
       : null;
+
+    console.log(movies);
     return (
       <div>
         <div className="u-sizeFull">
@@ -30,8 +33,22 @@ class Favorites extends Component {
           <span className="white f24"> / Favorites</span>
         </div>
         <h1 className="white fw6">Meus favoritos</h1>
-        <div className="u-sizeFull flex flex-column flex-row-ns flex-wrap-ns">
+        <div
+          className={
+            !movies.length
+              ? `u-sizeFull flex flex-column flex-row-ns flex-wrap-ns justify-center`
+              : `u-sizeFull flex flex-column flex-row-ns flex-wrap-ns `
+          }
+        >
           {movies}
+          {!movies.length && (
+            <div className="flex flex-column">
+              <img src={NotFound} alt="Not Found" />
+              <h1 className="white f16 f24-ns">
+                Não há filmes para serem exibidos
+              </h1>
+            </div>
+          )}
         </div>
       </div>
     );
