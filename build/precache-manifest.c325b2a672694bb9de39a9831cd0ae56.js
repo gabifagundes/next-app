712aae5b2b3465ac0af71cbf318eafc788f6cd93
gabifagundes <gabiipfagundes@gmail.com>
2019-02-10 14:35:13 -0200

    Run Deploy

---
+++ build/precache-manifest.c325b2a672694bb9de39a9831cd0ae56.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd9a326b110729265765cea128bb7ab",
+    "url": "/static/media/icon-heart-white.1dd9a326.svg"
+  },
+  {
+    "revision": "40bbcd10104acef1ea74",
+    "url": "/static/css/main.9e4c399d.chunk.css"
+  },
+  {
+    "revision": "bd668278c6196b155462",
+    "url": "/static/js/1.bd668278.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1b896970228fcd12a4ae587d2b5671d",
+    "url": "/static/media/not-found3.b1b89697.svg"
+  },
+  {
+    "revision": "bf7002d4239f262e44343cd663e95dc1",
+    "url": "/static/media/icon-arrow-white.bf7002d4.svg"
+  },
+  {
+    "revision": "40bbcd10104acef1ea74",
+    "url": "/static/js/main.40bbcd10.chunk.js"
+  },
+  {
+    "revision": "c2aef462b40620833f68316a29cba6a2",
+    "url": "/static/media/icon-heart-full.c2aef462.svg"
+  },
+  {
+    "revision": "08dd54b40059d0f7efc18644613d821b",
+    "url": "/static/media/logo.08dd54b4.svg"
+  },
+  {
+    "revision": "a620f9e9c28a6ca2ea4083e01d8ca0af",
+    "url": "/static/media/icon-heart-full-white.a620f9e9.svg"
+  },
+  {
+    "revision": "39a1dc2bd1062caeef0bc7faf32f9dd3",
+    "url": "/static/media/logo-imdb.39a1dc2b.svg"
+  },
+  {
+    "revision": "117225bf46fe70f27f53e01f06d86840",
+    "url": "/static/media/404.117225bf.svg"
+  },
+  {
+    "revision": "fc930a0fc851f3bfdb409024c3b37386",
+    "url": "/index.html"
+  }
+];
